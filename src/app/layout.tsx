1c327ed0cba397168a9cff2bdd4cc3cb306f3184
Philip Kaminski <philip.kaminski@roh.org.uk>
2026-02-13 15:41:23 +0000

    changed font to Fira Code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { PrismicPreview } from '@prismicio/next'
+import { Fira_Code } from 'next/font/google'
 import { repositoryName } from '@/prismicio'
 
 export const metadata: Metadata = {
@@ -8,13 +9,18 @@ export const metadata: Metadata = {
   description: 'RBO Developer Blog',
 }
 
+const firaCode = Fira_Code({
+  subsets: ['latin'],
+  variable: '--font-fira-code'
+})
+
 const RootLayout = ({
   children
 }: {
   children: React.ReactNode
 }) => (
   <html>
-    <body>
+    <body className={firaCode.variable}>
       <div>
         {children}
       </div>
