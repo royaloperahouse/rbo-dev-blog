474dbe534dbd22094379f188aa4e05ba3564d415
Philip Kaminski <philip.kaminski@roh.org.uk>
2026-02-27 16:13:53 +0000

    deleted comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,26 +16,10 @@ const Home = async () => {
   const posts = await client.getAllByType('dev_blog_article')
 
   return (
-    // <ContextProvider>
-    //   <div className='flex flex-col gap-3 disable-ligatures'>
-    //       <RboAsciiLogo literals={literalsArray} /> 
-    //     <div className='mx-[25%]'>
-    //       <Header title={page.data.title} subtitle={page.data.subtitle}/>
-    //     </div>
-    //     <div className='flex flex-row'>
-    //       <div className='w-[25%]'>
-    //         <Sidebar posts={posts}/>
-    //       </div>
-    //       <div>
-    //         <RichTextSlice className='mb-6' field={page.data.intro}/>
-    //         <ArticleList posts={posts}/>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </ContextProvider>
+
     <ContextProvider>
       <div className='flex flex-col gap-3 disable-ligatures'>
-          <RboAsciiLogo literals={literalsArray} /> 
+          <RboAsciiLogo literals={literalsArray} />
         <div className='flex flex-row mt-10'>
           <div className='w-[25%]'>
             <Sidebar posts={posts}/>
